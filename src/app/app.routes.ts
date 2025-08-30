478d5f6a943e0d76d47e32d45d17c68c865dc195
Vicente Louback <vicentethuler90@gmail.com>
2025-08-30 17:31:24 -0400

    Fazendo a estrutura do site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent}  from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: '', component: DashboardComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+
+    {path: 'home', component: HomeComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'tasks', component: TasksComponent}
 ];
