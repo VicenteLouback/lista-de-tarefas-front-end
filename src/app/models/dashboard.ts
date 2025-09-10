8942f0afa03af2fc7647d7b9bcc7ecbc15d35c27
Vicente Louback <vicentlouback@gmail.com>
2025-09-10 16:53:03 -0400

    layout e funções de adicionar tarefa

--- src/app/models/dashboard.ts
+++ src/app/models/dashboard.ts
@@ -1,2 +1,10 @@
 export interface Dashboard {
 }
+
+ export interface Tarefas{
+    titulo: string;
+    descricao: string;
+    data: Date;
+    prioridade: 'BAIXA' | 'MEDIA' | 'ALTA';
+    status: 'PENDENTE' | 'EM_ANDAMENTO' | 'CONCLUIDA';
+ }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Tarefas } from '../../models/dashboard';
+import { NgForOf } from '@angular/common';
+import { TasksComponent } from '../tasks/tasks.component';
+
 
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  tarefas: Tarefas[] = [];
+
+  constructor(private router: Router, private tarefaService: TasksComponent){}
+
+  ngOnInit(){
+    this.tarefas = this.tarefaService.listarTarefas();
+  }
+
+  navegarParaCriarTarefa(){
+    this.router.navigate(['/tasks']);
+  }
+
+  // Função para adicionar tarefas
+  adicionarTarefa(tarefa: Tarefas){
+    this.tarefas.push(tarefa);
+  }
+
+  //Contadores
+  get totalTarefas():number{
+    return this.tarefas.length;
+  }
+
+  get concluidas():number{
+    return this.tarefas.filter(t => t.status === 'CONCLUIDA').length;
+  }
+
+  get pendentes():number{
+    return this.tarefas.filter(t => t.status === 'PENDENTE').length;
+  }
+
+  get emAndamento(): number {
+    return this.tarefas.filter(t => t.status === 'EM_ANDAMENTO').length;
+  }
+  
+  
 }
